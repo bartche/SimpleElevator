92f808a3832b46973e227f52d3c4f0f2e644d3b9
bartche <91751131+bartche@users.noreply.github.com>
2025-03-28 13:30:39 -0300

    nova 0.18 compatibility

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,14 @@
-rootProject.name = "simple-elevator"
+rootProject.name = "simpleelevator"
 
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
-        maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://repo.papermc.io/repository/maven-public/")
         maven("https://repo.xenondevs.xyz/releases/")
     }
     versionCatalogs {
         create("libs") {
-            from("xyz.xenondevs.nova:catalog:0.17-alpha.11")
+            from("xyz.xenondevs.nova:catalog:0.18-alpha.12")
         }
     }
 }
@@ -17,7 +17,7 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         mavenCentral()
-        maven("https://papermc.io/repo/repository/maven-public/")
         maven("https://repo.xenondevs.xyz/releases/")
+		maven("https://repo.papermc.io/repository/maven-public/")
     }
 }
